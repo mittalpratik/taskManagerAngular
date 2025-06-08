f2de92f7d326f0fa005207cb90f138ec55033f9d
mittalpratik <42915677+mittalpratik@users.noreply.github.com>
2025-06-07 19:14:21 -0500

    Adding users,headers and task component

---
+++ 01-starting-project/src/app/user/user.component.ts
@@ -0,0 +1,26 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-user',
+  standalone: true,
+  imports: [],
+  templateUrl: './user.component.html',
+  styleUrl: './user.component.css'
+})
+export class UserComponent {
+@Input({ required : true}) user!:{
+  id : string,
+  name: string,
+  avatar: string
+};
+@Output() select = new EventEmitter<string>();
+
+get imagePath() {
+  return 'assets/users/' + this.user.avatar;
+}
+
+onSelectUser() {
+  this.select.emit(this.user.id);
+}
+
+}
